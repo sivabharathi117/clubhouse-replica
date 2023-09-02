011e2244dbca77b70586dd739a5f3849698880bf
SivaBharathi <sivabharathi.softsuave@gmail.com>
2023-09-02 18:15:08 +0530

    call feature implementation

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 var express = require("express");
 const router = require("./routes/router");
 var app = express();
-const port = 3000;
 var cors = require('cors');
+require('dotenv').config();
+const port = process.env.PORT || 3000;
+console.log(port);
+const peers = require('./webRtc/peers');
 
 app.use(cors());
 app.use(express.json());
 app.use('/api', router);
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+peers(server);
