30ac666ca7ca803718f6d5067959169316e669a8
SivaBharathi <sivabharathi.softsuave@gmail.com>
2023-10-19 11:26:12 +0530

    peer changes

--- webRtc/peers.js
+++ webRtc/peers.js
@@ -1,7 +1,7 @@
-var io = require('socket.io')
-    ({
-        path: '/io/webrtc'
-    });
+const io = require('socket.io')({ path: '/io/webrtc' });
+const roomsController = require('./../controllers/roomsController');
+const userController = require('./../controllers/userController');
+
 let connectedDevices = 0;
 const rooms = {}
 const messages = {}
@@ -13,153 +13,170 @@ function initializePeers(server) {
     });
 
     const peers = io.of('/webrtcPeer');
-    peers.on('connection', socket => {
-        console.log('peers connected')
-        connectedDevices += 1;
-        console.log('connected devices :' + connectedDevices);
-        const room = socket.handshake.query.room
-        console.log('rooooooooooom', room);
-
-        rooms[room] = rooms[room] && rooms[room].set(socket.id, socket) || (new Map()).set(socket.id, socket)
-        messages[room] = messages[room] || []
-
-        // console.log('rooooooom', rooms);
-        // console.log('messages', messages);
-
-        // connectedPeers.set(socket.id, socket)
-
-        // console.log(socket.id, room)
-        socket.emit('connection-success', {
-            success: socket.id,
-            peerCount: rooms[room].size,
-            messages: messages[room],
-        })
-
-        // const broadcast = () => socket.broadcast.emit('joined-peers', {
-        //   peerCount: connectedPeers.size,
-        // })
-        const broadcast = () => {
-            const _connectedPeers = rooms[room]
-
-            for (const [socketID, _socket] of _connectedPeers.entries()) {
-                // if (socketID !== socket.id) {
-                _socket.emit('joined-peers', {
-                    peerCount: rooms[room].size, //connectedPeers.size,
-                })
-                // }
-            }
-        }
-        broadcast()
-
-        // const disconnectedPeer = (socketID) => socket.broadcast.emit('peer-disconnected', {
-        //   peerCount: connectedPeers.size,
-        //   socketID: socketID
-        // })
-        const disconnectedPeer = (socketID) => {
-            const _connectedPeers = rooms[room]
-            for (const [_socketID, _socket] of _connectedPeers.entries()) {
-                _socket.emit('peer-disconnected', {
-                    peerCount: rooms[room].size,
-                    socketID
-                })
-            }
-        }
-
-        socket.on('new-message', (data) => {
-            // console.log('new-message', JSON.parse(data.payload))
-
-            messages[room] = [...messages[room], JSON.parse(data.payload)]
-        })
-
-        socket.on('disconnect', () => {
-            console.log('disconnected')
-            connectedDevices -= 1;
+    peers.on('connection', async socket => {
+        try {
+            console.log('peers connected')
+            connectedDevices += 1;
             console.log('connected devices :' + connectedDevices);
-            // connectedPeers.delete(socket.id)
-            rooms[room].delete(socket.id)
-            messages[room] = rooms[room].size === 0 ? null : messages[room]
-            disconnectedPeer(socket.id)
-        })
+            const room = socket.handshake.query.room || "kjhfbvghgfre11111";
+            const roomId = socket.handshake.query.roomId || 1;
+            const userId = socket.handshake.query.userId || 1;
+            socket.join(room)
+            console.log('rooooooooooom', room);
+            const joinRoom = await roomsController.joinRoom(userId, roomId, socket);
+            const userDetail = await userController.getUserDetail(userId) || {};
+            if (joinRoom) {
+                // rooms[room] = rooms[room] && rooms[room].set(socket.id, socket) || (new Map()).set(socket.id, socket)
+                // messages[room] = messages[room] || []
 
-        // ************************************* //
-        // NOT REQUIRED
-        // ************************************* //
-        // socket.on('socket-to-disconnect', (socketIDToDisconnect) => {
-        //   console.log('disconnected')
 
-        //   // connectedPeers.delete(socket.id)
-        //   rooms[room].delete(socketIDToDisconnect)
-        //   messages[room] = rooms[room].size === 0 ? null : messages[room]
-        //   disconnectedPeer(socketIDToDisconnect)
-        // })
+                // connectedPeers.set(socket.id, socket)
 
-        socket.on('onlinePeers', (data) => {
-            const _connectedPeers = rooms[room]
-            for (const [socketID, _socket] of _connectedPeers.entries()) {
-                // don't send to self
-                if (socketID !== data.socketID.local) {
-                    // console.log('online-peer', data.socketID, socketID)
-                    socket.emit('online-peer', socketID)
-                }
-            }
-        })
-
-        socket.on('offer', data => {
-            // console.log(data)
-            const _connectedPeers = rooms[room]
-            for (const [socketID, socket] of _connectedPeers.entries()) {
-                // don't send to self
-                if (socketID === data.socketID.remote) {
-                    // console.log('Offer', socketID, data.socketID, data.payload.type)
-                    socket.emit('offer', {
-                        sdp: data.payload.sdp,
-                        socketID: data.socketID.local
-                    }
-                    )
-                }
-            }
-        })
-
-        socket.on('answer', (data) => {
-            // console.log(data)
-            const _connectedPeers = rooms[room]
-            for (const [socketID, socket] of _connectedPeers.entries()) {
-                if (socketID === data.socketID.remote) {
-                    // console.log('Answer', socketID, data.socketID, data.payload.type)
-                    socket.emit('answer', {
-                        sdp: data.payload.sdp,
-                        socketID: data.socketID.local
-                    }
-                    )
-                }
-            }
-        })
-
-        // socket.on('offerOrAnswer', (data) => {
-        //   // send to the other peer(s) if any
-        //   for (const [socketID, socket] of connectedPeers.entries()) {
-        //     // don't send to self
-        //     if (socketID !== data.socketID) {
-        //       console.log(socketID, data.payload.type)
-        //       socket.emit('offerOrAnswer', data.payload)
-        //     }
-        //   }
-        // })
-
-        socket.on('candidate', (data) => {
-            // console.log(data)
-            const _connectedPeers = rooms[room]
-            // send candidate to the other peer(s) if any
-            for (const [socketID, socket] of _connectedPeers.entries()) {
-                if (socketID === data.socketID.remote) {
-                    socket.emit('candidate', {
-                        candidate: data.payload,
-                        socketID: data.socketID.local
+                const connectedUsers = await userController.getConnectedUsers(roomId);
+                if (connectedUsers) {
+                    socket.emit('connection-success', {
+                        success: socket.id,
+                        peerCount: connectedUsers.length
                     })
                 }
-            }
-        })
 
+
+                // const broadcast = () => socket.broadcast.emit('joined-peers', {
+                //   peerCount: connectedPeers.size,
+                // })
+                const broadcast = async () => {
+                    const _connectedUser = await userController.getConnectedUsers(roomId);
+
+                    // for (let user of _connectedUser) {
+                    // if (socketID !== socket.id) {
+                    // peers.to(room).emit('joined-peers', userDetail)
+                    peers.to(room).emit('joined-peers', {
+                        peerCount: _connectedUser.length, //connectedPeers.size,
+                    })
+                    // }
+                    // }
+                }
+                broadcast()
+
+                // const disconnectedPeer = (socketID) => socket.broadcast.emit('peer-disconnected', {
+                //   peerCount: connectedPeers.size,
+                //   socketID: socketID
+                // })
+                const disconnectedPeer = async (socketID, userDetail) => {
+                    const _connectedUser = await userController.getConnectedUsers(roomId);
+                    // for (const user of _connectedUser) {
+                    peers.to(room).emit('peer-disconnected', {
+                        peerCount: _connectedUser.length,
+                        socketID,
+                        username: userDetail.username,
+                        profilePic: userDetail.profile_pic,
+                        userId: userDetail.id
+                    })
+                    // }
+                }
+
+                socket.on('new-message', (data) => {
+
+                    // messages[room] = [...messages[room], JSON.parse(data.payload)]
+                })
+
+                socket.on('disconnect', async () => {
+                    try {
+                        console.log('disconnected')
+                        connectedDevices -= 1;
+                        console.log('connected devices :' + connectedDevices);
+                        socket.leave(room);
+                        // connectedPeers.delete(socket.id)
+                        const disconnectUser = await userController.disconnectUser(socket.id);
+                        if (disconnectUser) {
+                            disconnectedPeer(socket.id, disconnectUser)
+                        }
+                    } catch (err) {
+                        console.log(err)
+                    }
+
+                    // rooms[room].delete(socket.id)
+                    // messages[room] = rooms[room].size === 0 ? null : messages[room]
+
+                })
+
+
+                socket.on('onlinePeers', async (data) => {
+                    const _connectedUser = await userController.getConnectedUsers(roomId);
+                    for (let user of _connectedUser) {
+                        // don't send to self
+                        // if (socketID !== data.socketID.local) {
+                        socket.emit('online-peer',
+                            {
+                                socketID: user.socket_id,
+                                // username: user.username,
+                                // profilePic: user.profile_pic,
+                                userId: user.id
+                            }
+                        )
+                        // socket.emit('online-peer', user.socket.id)
+                        // }
+                    }
+                })
+
+                socket.on('offer', async data => {
+                    const _connectedUser = await userController.getConnectedUsers(roomId);
+                    for (let user of _connectedUser) {
+                        // don't send to self
+                        // if (user.socket_id === data.socketID.remote) {
+                        socket.emit('offer', {
+                            sdp: data.payload.sdp,
+                            socketID: data.socketID.local,
+                            userId: user.id
+                        }
+                        )
+                        // }
+                    }
+                })
+
+                socket.on('answer', async (data) => {
+                    const _connectedUser = await userController.getConnectedUsers(roomId);
+                    for (let user of _connectedUser) {
+                        if (user.socket_id === data.socketID.remote) {
+                            socket.emit('answer', {
+                                sdp: data.payload.sdp,
+                                socketID: data.socketID.local
+                            }
+                            )
+                        }
+                    }
+                })
+
+                socket.on('candidate', async (data) => {
+                    const _connectedUser = await userController.getConnectedUsers(roomId);
+                    // send candidate to the other peer(s) if any
+                    for (let user of _connectedUser) {
+                        if (user.socket_id === data.socketID.remote) {
+                            socket.emit('candidate', {
+                                candidate: data.payload,
+                                socketID: data.socketID.local
+                            })
+                        }
+                    }
+                })
+            }
+
+        } catch (e) {
+            console.log(e)
+            let error;
+            if (e.error && e.message) {
+                error = {
+                    error: e.error,
+                    message: e.message
+                }
+            } else {
+                error = {
+                    error: e,
+                    message: "something went wrong"
+                }
+            }
+            socket.emit('error', error)
+        }
     })
 }
 
